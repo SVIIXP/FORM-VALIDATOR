8f8b2b21db429d1fb9f5ddebc24c2d6af66cb24b
sviixp <saulo.pereira@gmail.com>
2021-03-03 16:10:23 -0300

    fineshed

--- scritp.js
+++ scritp.js
@@ -1,1 +1,92 @@
-alert("Hello World");
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+//Show input error message
+
+const showError = (input, message) => {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+};
+
+const showSuccess = (input, message) => {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+};
+
+// Check email is valid
+const checkEmail = (input) => {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, `Email is not valid`);
+  }
+};
+
+// Get field name
+const getFieldName = (input) => {
+  switch (input.id) {
+    case "username":
+      return "Username ";
+    case "email":
+      return "E-mail ";
+    case "password":
+    case "password2":
+      return "Password ";
+    default:
+      break;
+  }
+};
+
+// Check required fields
+const checkRequired = (inputArr) => {
+  inputArr.forEach((input) => {
+    if (input.value == "") {
+      showError(input, `${getFieldName(input)}is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+};
+
+// Check input length
+const checkLength = (input, min, max) => {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must not have more than ${max} characters`
+    );
+  } else {
+    showSuccess(input);
+  }
+};
+
+// chech passwords match
+
+const passwordsMatch = (input1, input2) => {
+  console.log(input1.value, input2.value);
+  if (input1.value != input2.value) {
+    showError(input2, "Passwords don't match.");
+  }
+};
+
+// Event listeners
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  passwordsMatch(password, password2);
+});
